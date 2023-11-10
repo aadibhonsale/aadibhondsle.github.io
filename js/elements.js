55c01e7b65daba18641c967b33821859198ceb1b
QuantumByteStudios <quantumbytestudios@gmail.com>
2023-11-10 19:36:59 +0530

    Update contact information and footer content

--- js/elements.js
+++ js/elements.js
@@ -1,5 +1,4 @@
 footer = `
-
 	<footer class="ftco-footer ftco-section">
 		<div class="container">
 			<div class="row mb-5">
@@ -7,11 +6,11 @@ footer = `
 					<div class="ftco-footer-widget mb-4">
 						<h2 class="ftco-heading-2">About Me</h2>
 						<p>
-							Lorem, ipsum dolor sit amet consectetur adipisicing elit. Maiores, esse fugiat enim vero ducimus magni incidunt ipsam facere exercitationem nesciunt ut sed nulla itaque veritatis voluptas autem qui explicabo vitae?
+							hi, i’m aadi a enthusiastic artist, photographer and content creator. currently studying architecture at SEA-school of environment and architecture. i love trekking and travelling.
 						</p>
 						<ul class="ftco-footer-social list-unstyled float-md-left float-lft mt-5">
 							<li class="ftco-animate"><a href="#"><span class="icon-twitter"></span></a></li>
-							<li class="ftco-animate"><a href="#"><span class="icon-facebook"></span></a></li>
+							<!-- <li class="ftco-animate"><a href="#"><span class="icon-facebook"></span></a></li> -->
 							<li class="ftco-animate"><a href="#"><span class="icon-instagram"></span></a></li>
 						</ul>
 					</div>
@@ -20,9 +19,9 @@ footer = `
 					<div class="ftco-footer-widget mb-4 ml-md-5">
 						<h2 class="ftco-heading-2">Information</h2>
 						<ul class="list-unstyled">
-							<li><a href="#" class="py-2 d-block">Portfolio</a></li>
-							<li><a href="#" class="py-2 d-block">Privacy</a></li>
-							<li><a href="#" class="py-2 d-block">Terms Condition</a></li>
+							<li><a href="." class="py-2 d-block">Portfolio</a></li>
+							<!-- <li><a href="#" class="py-2 d-block">Privacy</a></li> -->
+							<!-- <li><a href="#" class="py-2 d-block">Terms Condition</a></li> -->
 						</ul>
 					</div>
 				</div>
@@ -30,10 +29,10 @@ footer = `
 					<div class="ftco-footer-widget mb-4">
 						<h2 class="ftco-heading-2">Links</h2>
 						<ul class="list-unstyled">
-							<li><a href="#" class="py-2 d-block">Home</a></li>
-							<li><a href="#" class="py-2 d-block">About</a></li>
-							<li><a href="#" class="py-2 d-block">Work</a></li>
-							<li><a href="#" class="py-2 d-block">Contact</a></li>
+							<li><a href="." class="py-2 d-block">Home</a></li>
+							<!-- <li><a href="#" class="py-2 d-block">About</a></li> -->
+							<li><a href="work.html" class="py-2 d-block">Work</a></li>
+							<li><a href="contact.html" class="py-2 d-block">Contact</a></li>
 						</ul>
 					</div>
 				</div>
@@ -42,12 +41,11 @@ footer = `
 						<h2 class="ftco-heading-2">Have a Questions?</h2>
 						<div class="block-23 mb-3">
 							<ul>
-								<li><span class="icon icon-map-marker"></span><span class="text">203 Fake St. Mountain
-										View, San Francisco, California, USA</span></li>
-								<li><a href="#"><span class="icon icon-phone"></span><span class="text">+2 392 3929
-											210</span></a></li>
-								<li><a href="#"><span class="icon icon-envelope"></span><span
-											class="text">info@yourdomain.com</span></a></li>
+								<!-- <li><span class="icon icon-map-marker"></span><span class="text">203 Fake St. Mountain
+										View, San Francisco, California, USA</span></li> -->
+								<li><a href="tel:+919422014920"><span class="icon icon-phone"></span><span class="text">+91 9422014920</span></a></li>
+								<li><a href="mailto:a23aadi@sea.edu.in"><span class="icon icon-envelope"></span><span
+											class="text">a23aadi@sea.edu.in</span></a></li>
 							</ul>
 						</div>
 					</div>
@@ -55,7 +53,6 @@ footer = `
 			</div>
 			<div class="row">
 				<div class="col-md-12 text-center">
-
 					<p>
 						Copyright &copy;
 						<script>document.write(new Date().getFullYear());</script> All rights reserved
@@ -68,7 +65,6 @@ footer = `
 `;
 
 navbar = `
-
 	<!-- START nav -->
 	<nav class="navbar navbar-expand-lg navbar-dark ftco_navbar bg-dark ftco-navbar-light" id="ftco-navbar">
 		<div class="container">
@@ -88,5 +84,4 @@ navbar = `
 		</div>
 	</nav>
 	<!-- END nav -->
-
 `;
